d5ccf9f42a50e43dc338358fa8fd2071c182b75f
Pascal Sommer <p@pascalsommer.ch>
2016-04-14 00:51:53 +0200

    Email obfuscation

---
+++ js/click.js
@@ -0,0 +1,7 @@
+$(function(){
+	$(".email").click(function(){
+		var stringA = "mailto:contac";
+		var stringB = "t@digitalwing.ch";
+		window.location.href = stringA + stringB;
+	});
+});
---
+++ js/loadup.js
@@ -0,0 +1,5 @@
+$(function(){
+	var viewportHeight = window.innerHeight;
+	$("header").css("height", viewportHeight + "px");
+	$(".section").css("min-height", viewportHeight + "px");
+})
