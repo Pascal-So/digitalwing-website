7ac3d2a7fcc341d509128c6a34a22d863cd61889
DeinFreund <deinbestrfreund@gmail.com>
2016-04-15 21:00:27 +0200

    slighltly delay height hardcoding because weird chrome behaviour

--- js/loadup.js
+++ js/loadup.js
@@ -1,5 +1,8 @@
 $(function(){
-	var viewportHeight = window.innerHeight;
-	$("header").css("height", viewportHeight + "px");
-	$(".section").css("min-height", viewportHeight + "px");
+	setTimeout(function(){
+		var viewportHeight = window.innerHeight;
+		$("header").css("height", viewportHeight + "px");
+		$(".section").css("min-height", viewportHeight + "px");
+	
+	}, 500);
 })
