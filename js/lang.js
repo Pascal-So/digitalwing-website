c457db9a37105a4da9661ef877b7275d60be6c22
DeinFreund <deinbestrfreund@gmail.com>
2016-04-16 20:52:32 +0200

    updated translation

--- js/lang.js
+++ js/lang.js
@@ -5,16 +5,16 @@ $(function(){
 	var titleAbout = "&nbsp;&Uuml;ber uns&nbsp;&nbsp;";
 	
 	var skillDynamicTitle = "Bewegte Aufnahmen";
-	var skillDynamicText = "Keine Lust mehr auf feste Perspektiven von einem Stativ in der Ecke des Raums? Wir finden nicht nur f&uuml;r jede Aufnahme eine andere Position, sondern k&ouml;nnen auch fl&uuml;ssige Kamerabewegungen bieten. Damit wirkt die Action umso mehr. Dank unserer Erfahrung entdecken wir die spannendsten Winkel ohne einen Moment zu verpassen.";
+	var skillDynamicText = "Keine Lust mehr auf feste Perspektiven von einem Stativ in der Ecke des Raums? Wir finden für jeden Clip eine neue Position. Übergänge ersetzen wir durch flüssige Kamerabewegungen. Damit wird die Wirkung des Geschehens noch verstärkt. Dank unserer Erfahrung entdecken wir die spannendsten Winkel ohne einen Moment zu verpassen.";
 	
 	var skillLowLightTitle = "Verf&uuml;gbares Licht";
-	var skillLowLightText = "Von Anfang mussten wir uns an schwache und anderweitig suboptimale Lichtverh&auml;ltnisse gew&ouml;hnen. Deswegen k&ouml;nnen wir jetzt die Stimmung in der Disko oder die Ambiente der Mondlichtszene realit&auml;tsgetreu reproduzieren - ganz ohne zus&auml;tzliches Licht. F&uuml;r uns z&auml;hlt Realismus mehr als jedes kleine Detail k&uuml;nstlich perfektioniert zu haben.";
+	var skillLowLightText = "Die Stimmung in der Disko oder das Ambiente der Mondlichtszene sind massgebend für den Erfolg Ihres Events. Für die Filmtechnik sind dies suboptimale Lichtverhältnisse. Jedoch zählt für uns Realismus mehr als jedes kleine Detail. Ohne künstlichen Perfektionismus und ohne unnötige Beleuchtung bieten wir Ihnen die realitätsnahe Wiedergabe der vorhandenen Stimmung.";
 	
 	var skillEditingTitle = "Schnitt";
-	var skillEditingText = "Ob actiongeladener Werbespot oder schlichter Dokumentarfilm, unsere Erfahrung in zwei- and dreidimensionalen visuellen Effekten bereichert Ihre Produktion. Wenn es um den letzten Schliff Ihres Projekts geht, besorgen wir Ihnen gerne einen individuellen Soundtrack und peppen das Material mit einer lebhaften Farbkorrektur auf.";
+	var skillEditingText = "Ob actiongeladener Werbespot oder schlichter Dokumentarfilm, unsere Erfahrung in zwei- and dreidimensionalen visuellen Effekten bereichert Ihre Produktion. Wenn es um den letzten Schliff Ihres Projekts geht, offerieren/anbieten wir Ihnen gerne einen individuellen Soundtrack und beleben das Material mit einer wirksamen Farbkorrektur.";
 	
 	var skillFlexibleTitle = "Flexibilit&auml;t";
-	var skillFlexibleText = "Dank unserer leichten Ausr&uuml;stung k&ouml;nnen wir jeden Ort einfach erreichen. Unsere Verbindungen mit anderen Filmemachern erlauben es uns, jederzeit weitere Spezialisten in Anspruch zu nehmen. Bei uns z&aumlhlt nur, ob am Ende die gew&uuml;nschten Aufnahmen vorhanden sind."
+	var skillFlexibleText = "Wir sind mit einer modernen und vorteilhaft mobilen Technik ausgestattet. Auch sehr belebte Events oder landschaftlich entlegene Orte können wir erreichen. In der Community der Filmemachern sind wir gut vernetzt. Bei Bedarf und nach Absprache mit Ihnennehmen wir weitere Spezialisten in Anspruch . Bei uns zählt, dass Sie am Ende die gewünschte Aufnahme erhalten."
 	
 	var projectColumn0Text = "Wir freuen uns auf eine produktive Zusammenarbeit, senden Sie uns doch gleich eine <span class='email'>E-Mail</span>!";
 	var projectColumn1Text = "";
