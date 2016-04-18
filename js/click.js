2c61c828b32721ae0eb6f288cf1713e7383a74a1
DeinFreund <deinbestrfreund@gmail.com>
2016-04-18 16:56:26 +0200

    recent projects

--- js/click.js
+++ js/click.js
@@ -1,42 +1,44 @@
 $(function(){
-	$(".email").click(function(){
-		var stringA = "mailto:contac";
-		var stringB = "t@digitalwing.ch";
-		window.location.href = stringA + stringB;
-	});
-
-
-	$(".skillLink").click(function(e){
-		if(window.innerWidth < 750){
-			return;
-		}
-		$("#ytOverlay").removeClass("invisible");
-		$("#screenDarkener").removeClass("invisible");
-		var options = "autoplay=1&rel=0&enablejsapi=1";
-		var id = $(this).attr('id');
-		var padding = $(this).attr('aspect');
-		$("#ytOverlay").css("padding-bottom", padding);
-		$("#hereBePlayer").html("<iframe src=\"https://www.youtube.com/embed/" + id + "?" + options + "\" frameborder=\"0\" allowfullscreen></iframe>");
-
-	});
-
 	
-	$("#closeButton").click(function(){
-		closeOverlay();
-	})
+	setTimeout(function(){
+		$(".email").click(function(){
+			var stringA = "mailto:contac";
+			var stringB = "t@digitalwing.ch";
+			window.location.href = stringA + stringB;
+		});
 
-	$(document).keydown(function(e){
-		if(e.keyCode == 27){
+
+		$(".ytLink").click(function(e){
+			if(window.innerWidth < 750){
+				return;
+			}
+			$("#ytOverlay").removeClass("invisible");
+			$("#screenDarkener").removeClass("invisible");
+			var options = "autoplay=1&rel=0&enablejsapi=1";
+			var id = $(this).attr('id');
+			var padding = $(this).attr('aspect');
+			$("#ytOverlay").css("padding-bottom", padding);
+			$("#hereBePlayer").html("<iframe src=\"https://www.youtube.com/embed/" + id + "?" + options + "\" frameborder=\"0\" allowfullscreen></iframe>");
+
+		});
+
+		
+		$("#closeButton").click(function(){
 			closeOverlay();
+		})
+
+		$(document).keydown(function(e){
+			if(e.keyCode == 27){
+				closeOverlay();
+			}
+		});
+
+		function closeOverlay(){
+			$("#hereBePlayer").html("");
+			$("#screenDarkener").addClass("invisible");
+			$("#ytOverlay").addClass("invisible");
 		}
-	});
-
-	function closeOverlay(){
-		$("#hereBePlayer").html("");
-		$("#screenDarkener").addClass("invisible");
-		$("#ytOverlay").addClass("invisible");
-	}
-
+	}, 50);
 	//
 
 });
--- js/loadup.js
+++ js/loadup.js
@@ -8,7 +8,13 @@ $(function(){
 		}, 500);
 	});
 	
-	$(".skillLink").attr("href", "#skills");
-	$(".navlink > a").attr("href", "#title");
 
+	setTimeout(function(){
+		if(window.innerWidth >= 750){
+			$(".skillLink").attr("href", "#skills");
+			$(".aboutLink").attr("href", "#about");
+			$(".aboutLink").attr("target", "");
+		}
+		$(".navlink > a").attr("href", "#title");
+	}, 50);
 });
